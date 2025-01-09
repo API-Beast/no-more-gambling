a8a2d515d2d1c41c468315d10de4e708f65a5b51
Beast <api.beast@gmail.com>
2025-01-09 21:48:07 +0100

    Blacklist Maraxis sand extractors.

--- blacklists.lua
+++ blacklists.lua
@@ -5,4 +5,8 @@ if mods["promethium-quality"] then
 	category_blacklist["refining"] = true
 	machine_blacklist["refinery"] = true
 end
+if mods["maraxsis"] then
+	machine_blacklist["electric-mining-drill-sand-extractor"] = true
+	machine_blacklist["big-mining-drill-sand-extractor"] = true
+end
 return {category_blacklist, machine_blacklist}
